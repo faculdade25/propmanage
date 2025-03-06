7bf4db747dd2b6a7b8622f82f5b4dc1d7c252d2e
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2025-03-05 22:55:55 -0300

    add initial UI components and directives for Helm design system

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginMenuComponent } from '../../block/login-menu';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoginMenuComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
