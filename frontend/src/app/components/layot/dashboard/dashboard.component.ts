f056eef952474b82138b5b3ecc299363af0aca1a
backupnobre62 <backupnobre62@gmail.com>
2025-03-09 19:11:44 +0000

    Melhoria de UI

--- frontend/src/app/components/layot/dashboard/dashboard.component.ts
+++ frontend/src/app/components/layot/dashboard/dashboard.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {
+  HlmCaptionComponent,
+  HlmTableComponent,
+  HlmTdComponent,
+  HlmThComponent,
+  HlmTrowComponent,
+} from '@spartan-ng/ui-table-helm';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HlmTableComponent, HlmTrowComponent, HlmThComponent, HlmTdComponent, HlmCaptionComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  userName: string = 'User';
 
 }
