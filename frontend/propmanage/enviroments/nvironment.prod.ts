ba57fcfc94815d718b574c1b66c8b244c917128e
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2025-03-12 12:50:32 +0000

    Adiciona propriedade

--- frontend/propmanage/enviroments/nvironment.prod.ts
+++ frontend/propmanage/enviroments/nvironment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://improved-space-succotash-576p5476pp727946-8080.app.github.dev'
+  apiUrl: 'https://improved-space-succotash-576p5476pp727946-8080.app.github.dev',
+  test: "test"
 };
